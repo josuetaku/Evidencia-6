6e9baf102f5ba0764a2df832391516d26da3a757
josuetaku <67020484+josuetaku@users.noreply.github.com>
2023-10-28 22:34:25 -0400

    Ejercicio 4

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
-const num = document.getElementById('input');
-const btn = document.querySelector('button');
+var btn = document.getElementById("btn");
+var modal = document.getElementById("modal");
+var modalTitle = document.getElementById("modalTitle");
+var btnClose = document.getElementById("btnClose");
+var textModal =  document.getElementById("textModal");
+var close = document.getElementById("close");
 
-btn.addEventListener('click', function compEdad(){
-    if (num.value >= 18) {
-        alert('Bienvenido, usted tiene acceso a todos nuestros servicios.');
-    } else {
-        alert('Bienvenido, usted tiene acceso a servicios limitados.');
-    }   
-});
+
+    btn.onclick = function () {
+        let num = prompt("Inserte un numero entre 1-10");
+        let result = "";
+        // El titulo del modal sera: Tabla del "numero insertado" 
+        modalTitle.innerHTML = "Tabla del " + num;
+        if (num >= 1 && num <= 10) {
+            for (let i = 0; i <= 12; i++) {
+                result += num + " x " + i + " = " + num * i + "<br>";
+            }
+            // Con esto se mostrara la tabla el modal
+            textModal.innerHTML = result;
+            modal.style.display = "block";
+        } else {
+            // En caso de que el usuario no inserte un numero admitido
+            alert("El valor digitado no es admitido, por favor inserte un valor del 1-10.");
+        }
+
+        // Boton cerrar al final del modalbox
+        btnClose.onclick = function () {
+            modal.style.display = "none";
+        };
+
+        // "x" al inicio del modalbox, igual para cerrarlo
+        close.onclick = function() {
+            modal.style.display = "none";
+        }
+
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        };
+    }
