967df7f8be8d6e1f82a1c08bedb09fc16f06ba08
josuetaku <67020484+josuetaku@users.noreply.github.com>
2023-10-28 22:34:46 -0400

    Ejercicio 5

--- script.js
+++ script.js
@@ -1,41 +1,51 @@
-var btn = document.getElementById("btn");
-var modal = document.getElementById("modal");
-var modalTitle = document.getElementById("modalTitle");
-var btnClose = document.getElementById("btnClose");
-var textModal =  document.getElementById("textModal");
-var close = document.getElementById("close");
+window.onload = function() {
+    let selector = document.getElementById('select');
+    let resultado = document.getElementById('result');
 
+    selector.addEventListener('change', function() {
 
-    btn.onclick = function () {
-        let num = prompt("Inserte un numero entre 1-10");
-        let result = "";
-        // El titulo del modal sera: Tabla del "numero insertado" 
-        modalTitle.innerHTML = "Tabla del " + num;
-        if (num >= 1 && num <= 10) {
-            for (let i = 0; i <= 12; i++) {
-                result += num + " x " + i + " = " + num * i + "<br>";
-            }
-            // Con esto se mostrara la tabla el modal
-            textModal.innerHTML = result;
-            modal.style.display = "block";
-        } else {
-            // En caso de que el usuario no inserte un numero admitido
-            alert("El valor digitado no es admitido, por favor inserte un valor del 1-10.");
+        resultado.innerHTML = '';
+        
+        //Llamar la opcion seleccionada para mostrar su valor
+        let tabla = this.value;
+
+        if(tabla == 0){
+            return;
+        }
+        
+        let table = document.createElement('table');
+        let thead = document.createElement('thead');
+        let tbody = document.createElement('tbody');
+        let headRow = document.createElement('tr');
+        
+        ["Multiplicando", "Multiplicador", "Resultado"].forEach(function(text) {
+            let th = document.createElement('th');
+            th.textContent = text;
+            headRow.appendChild(th);
+        });
+        
+        thead.appendChild(headRow);
+        table.appendChild(thead);
+        
+        // Crea y muestra la tabla seleccionada
+        for (let i = 1; i <= 12; i++) {
+            let tr = document.createElement('tr');
+            let td1 = document.createElement('td');
+            let td2 = document.createElement('td');
+            let td3 = document.createElement('td');
+
+            td1.textContent = tabla;
+            td2.textContent = i;
+            td3.textContent = tabla * i;
+
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            tr.appendChild(td3);
+
+            tbody.appendChild(tr);
         }
 
-        // Boton cerrar al final del modalbox
-        btnClose.onclick = function () {
-            modal.style.display = "none";
-        };
-
-        // "x" al inicio del modalbox, igual para cerrarlo
-        close.onclick = function() {
-            modal.style.display = "none";
-        }
-
-        window.onclick = function (event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        };
-    }
+        table.appendChild(tbody);
+        resultado.appendChild(table);
+    });
+};
